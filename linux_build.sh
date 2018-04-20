05479aba2150b9e7442eff879a13e44fae90c8ed
erick cafferata <erick@cafferata.me>
2018-04-20 14:52:25 -0500

    add linux_build.sh
    - script used to build any version of mainline kernel or (optionally)
      patched from kernel.org.
    - intended to work with linux_MakeExport.sh

---
+++ linux_build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Takes a tag snapshot of the linux kernel and (optionally) patches it from kernel.org
+# Builds it using a selected .config file.
+# This scripts automates half of the offsite kernel building process,
+# and it's intended to work along with linux_MakeExport.sh.
